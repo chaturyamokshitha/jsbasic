4ce7bae9323015a6a8ab13da166dad7735b332fb
chaturyamokshitha <127294370+chaturyamokshitha@users.noreply.github.com>
2023-09-07 22:24:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-let a = 0,
-  b = 1,
-  c;
-let i = 0;
+//febonacci series
+let a = 0, b = 1,c,i;
 c = a + b;
 console.log(a, b);
 for (i = 3; i <= 5; i++) {
@@ -10,3 +8,10 @@ for (i = 3; i <= 5; i++) {
   b = c;
   c = a + b;
 }
+//factorial of number 5
+let f=1,i;
+for(i=1;i<=5;i++){
+    f=f*i;
+}
+console.log(f);
+
